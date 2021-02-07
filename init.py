5fd57f0fbdeefc9fd73d60e5270283eefef7448c
Naveed Gazi <naveed@Naveeds-MacBook-Pro-2.local>
2021-02-07 14:31:13 -0500

    first commit

---
+++ init.py
@@ -0,0 +1,14 @@
+import time
+import sys
+
+i = 0
+
+while 1:
+    s = "_" * i + "-" + "_" * (10 - i)
+    print(s, end='\r')
+    sys.stdout.flush()
+    i += 1
+    i %= 10
+    time.sleep(1)
+
+#modify file
